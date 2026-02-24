38c4640ed9524cba50bb9fe7f5f33f4e7e50d368
riesling <riesling.alsace@gmail.com>
2026-02-24 18:22:15 +0900

    Coreの分離

---
+++ MAUI/ViewModelBase.cs
@@ -0,0 +1,64 @@
+﻿using System.ComponentModel;
+
+using Microsoft.Maui.ApplicationModel;
+
+namespace Riesling.Library.Mvvms.MAUI;
+
+public class ViewModelBase : BindableBase {
+
+	#region Events
+
+	protected override void RaisePropertiesChanged(params string[] propertyNames) {
+		if (MainThread.IsMainThread) {
+			base.RaisePropertiesChanged(propertyNames);
+		} else {
+			MainThread.BeginInvokeOnMainThread(() => {
+				lock (this) {
+					base.RaisePropertiesChanged(propertyNames);
+				}
+			});
+		}
+	}
+
+	#endregion
+
+}
+
+public abstract class ViewModelBase<TModelBase> : ViewModelBase
+	where TModelBase : BindableBase {
+
+	#region Instances
+
+	protected TModelBase _Model;
+
+	#endregion
+
+	#region Properties
+
+	public TModelBase Model {
+		get => _Model;
+		set => SetProperty(ref _Model, value, Model_Changed);
+	}
+
+	protected virtual void Model_Changed(TModelBase newModel, TModelBase oldModel) {
+		if (oldModel != null) {
+			oldModel.PropertyChanged -= Model_PropertyChanged;
+		}
+		if (newModel != null) {
+			newModel.PropertyChanged += Model_PropertyChanged;
+		}
+	}
+
+	#endregion
+
+	#region Constructor
+
+	public ViewModelBase(TModelBase model) {
+		Model = model;
+	}
+
+	protected abstract void Model_PropertyChanged(object sender, PropertyChangedEventArgs e);
+
+	#endregion
+
+}
